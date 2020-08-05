0e748468f6fb98c30feb581578f9d01a98492413
muhammadumerhack <umer1807f@aptechsite.net>
2020-08-06 04:10:11 +0500

    aaa

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello Kese ho janu <code>src/App.js</code> and save to reload.
+         Hello Kese ho ssjanud <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
