f2e41e36216989fa9e0b92695a631d9d526922b6
muhammadumerhack <umer1807f@aptechsite.net>
2020-08-08 16:07:09 +0500

    Meeting with Kyoade

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello Zaeem How how are you <code>src/App.js</code> and save to reload.
+         Hello Kyode How how are you <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
